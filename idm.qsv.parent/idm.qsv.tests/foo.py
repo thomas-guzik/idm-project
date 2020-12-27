5addccd5712c03138785e37153b8d277d7ba5948
Sigrid <gimau>
2020-12-27 13:25:18 +0100

    Merge

--- idm.qsv.parent/idm.qsv.tests/foo.py
+++ idm.qsv.parent/idm.qsv.tests/foo.py
@@ -10,7 +10,9 @@ def printData(data):
     print(data)
 
 my_data = pd.read_csv("foo_numbers.csv", header='infer')
-sumCol0Col1 = my_data[["col0", "col1"]].sum(axis=1)
-my_data.insert(loc=len(my_data.columns), column="newcol", value=sumCol0Col1)
-tmp1 = my_data
-printData(tmp1)
+filter71 = my_data["col1"] < 5
+filter70 = filter71
+my_data = my_data.drop(my_data[filter70].index)
+
+tmp54 = my_data
+printData(tmp54)
--- idm.qsv.parent/idm.qsv/xtend-gen/idm/compiler/BashCompiler.java
+++ idm.qsv.parent/idm.qsv/xtend-gen/idm/compiler/BashCompiler.java
@@ -3,9 +3,13 @@ package idm.compiler;
 import com.google.common.io.Files;
 import idm.compiler.TerminalOutput;
 import idm.qsv.Column;
-import idm.qsv.ColumnName;
+import idm.qsv.ColumnNames;
+import idm.qsv.ColumnNumbers;
 import idm.qsv.Columns;
+import idm.qsv.Condition;
 import idm.qsv.Header;
+import idm.qsv.Line;
+import idm.qsv.LineRange;
 import idm.qsv.Lines;
 import idm.qsv.Print;
 import idm.qsv.QuerySeparatedValues;
@@ -15,9 +19,11 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.List;
 import java.util.function.Consumer;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.xtend2.lib.StringConcatenation;
+import org.eclipse.xtext.xbase.lib.Conversions;
 import org.eclipse.xtext.xbase.lib.Exceptions;
 import org.eclipse.xtext.xbase.lib.InputOutput;
 
@@ -67,7 +73,7 @@ public class BashCompiler {
   
   public String compile(final Header header) {
     String code = "";
-    String nameFile = this.qsv.getHeader().getNameFile();
+    String nameFile = header.getNameFile();
     this.hasColumnName = Boolean.valueOf(header.isHasColumnName());
     this.separator = ",";
     String _code = code;
@@ -96,40 +102,106 @@ public class BashCompiler {
   }
   
   public String compile(final Print print) {
+    this.initializeVariable();
     this.compile(print.getSelector());
-    return "rien";
+    return this.generatePrintCode();
   }
   
-  public Object compile(final Selector selector) {
-    Object _xblockexpression = null;
+  public String[] initializeVariable() {
+    String[] _xblockexpression = null;
     {
-      Columns columnSelection = selector.getColumnSelection();
-      Lines lineSelection = selector.getLineSelection();
-      if ((columnSelection == null)) {
-        this.cols = new String[] {};
-      } else {
-        final Consumer<Column> _function = (Column e) -> {
-          this.compile(e);
-        };
-        columnSelection.getColumns().forEach(_function);
-      }
-      Object _xifexpression = null;
-      if ((lineSelection == null)) {
-        _xifexpression = null;
-      } else {
-        _xifexpression = null;
-      }
-      _xblockexpression = _xifexpression;
+      this.cols = new String[] {};
+      _xblockexpression = this.lines = new String[] {};
     }
     return _xblockexpression;
+  }
+  
+  public String compile(final Selector selector) {
+    String code = "";
+    Columns columnSelection = selector.getColumnSelection();
+    Lines lineSelection = selector.getLineSelection();
+    if ((columnSelection != null)) {
+      Column columns = columnSelection.getColumns();
+      if ((columns == null)) {
+        this.cols = new String[] {};
+      } else {
+        this.compile(columns);
+      }
+    }
+    if ((lineSelection != null)) {
+      LineRange range = lineSelection.getRange();
+      Line line = lineSelection.getLine();
+      Condition cond = lineSelection.getCond();
+    }
+    return code;
   }
   
   public String compile(final Column c) {
     return InputOutput.<String>println("what happend ?");
   }
   
-  public Object compile(final ColumnName c) {
-    return null;
+  public void compile(final ColumnNames c) {
+    EList<String> names = c.getNames();
+    final Consumer<String> _function = (String n) -> {
+      ((List<String>)Conversions.doWrapArray(this.cols)).add(n);
+    };
+    names.forEach(_function);
+  }
+  
+  public void compile(final ColumnNumbers c) {
+    EList<String> numbers = c.getNumbers();
+    final Consumer<String> _function = (String n) -> {
+      ((List<String>)Conversions.doWrapArray(this.cols)).add(n);
+    };
+    numbers.forEach(_function);
+  }
+  
+  public String generatePrintCode() {
+    String code = "n=0\n";
+    String _code = code;
+    code = (_code + "while read");
+    String colCode = "";
+    boolean _isEmpty = ((List<String>)Conversions.doWrapArray(this.cols)).isEmpty();
+    if (_isEmpty) {
+      String _code_1 = code;
+      code = (_code_1 + " line\n");
+      String _colCode = colCode;
+      colCode = (_colCode + "echo $line\n");
+    } else {
+      String _colCode_1 = colCode;
+      colCode = (_colCode_1 + "");
+    }
+    String _code_2 = code;
+    code = (_code_2 + "do\n");
+    boolean _isEmpty_1 = ((List<String>)Conversions.doWrapArray(this.lines)).isEmpty();
+    if (_isEmpty_1) {
+      String _code_3 = code;
+      code = (_code_3 + colCode);
+    } else {
+      int _size = ((List<String>)Conversions.doWrapArray(this.lines)).size();
+      boolean _equals = (_size == 1);
+      if (_equals) {
+        String _code_4 = code;
+        StringConcatenation _builder = new StringConcatenation();
+        _builder.append("if[ $n -eq ");
+        String _get = this.lines[0];
+        _builder.append(_get);
+        _builder.append(" ]");
+        code = (_code_4 + _builder);
+      } else {
+        String _code_5 = code;
+        code = (_code_5 + "if ");
+      }
+      String _code_6 = code;
+      code = (_code_6 + "then\n");
+      String _code_7 = code;
+      code = (_code_7 + colCode);
+      String _code_8 = code;
+      code = (_code_8 + "fi\n");
+    }
+    String _code_9 = code;
+    code = (_code_9 + "done");
+    return code;
   }
   
   public TerminalOutput executeOnFile(final String code, final String filename) {
