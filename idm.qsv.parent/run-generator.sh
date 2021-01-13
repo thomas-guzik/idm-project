f80f8ac4f80cfb248857bbc581368d4df47d2727
Sigrid <sigrid.droogh@laposte.net>
2021-01-13 11:35:13 +0100

    Generate for multiple files

--- idm.qsv.parent/run-generator.sh
+++ idm.qsv.parent/run-generator.sh
@@ -1,6 +1,27 @@
 #! /bin/bash
 
-java -jar idm.qsv/build/libs/idm.qsv-1.0.0-SNAPSHOT.jar test.qsv
+if [ $# -lt 1 ];then
+    echo "Please provide at least one QSV file"
+    exit 1
+fi
 
-echo "Output code:"
-cat src-gen/greetings.txt
+for file in "$@"; do
+    if [[ ! -f "$file" ]]; then
+        echo "File $file does not exist"
+        exit 1
+    fi
+done
+
+echo "Found all QSV files."
+
+for file in "$@"; do
+    echo "Processing $file ..."
+    java -jar idm.qsv/build/libs/idm.qsv-1.0.0-SNAPSHOT.jar test.qsv
+    echo "Generated code:"
+    echo
+    cat src-gen/greetings.txt
+    echo
+    echo
+done
+
+echo "Finished"
